1702039d820d713578d9fa93e6e56a1dd63411cc
Majd Hamde <majdhamde1901@gmail.com>
2026-02-10 11:10:58 +0100

    feat(auth): add frontend login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
+import Login from "./pages/User/Login";
 import UserList from "./pages/User/UserList";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<UserList />} />
         <Route path="/signup" element={<CreateUser />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
